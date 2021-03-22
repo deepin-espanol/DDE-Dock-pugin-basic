9a99b0f45ab10b8250e87b25a1ec19c8891307b8
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2021-03-21 19:34:38 -0930

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# use /usr/lib/dde-dock/plugins/system-trays to use as tray instead of system plugins part
 sudo cp `dirname $0`/libbasicplugin.so /usr/lib/dde-dock/plugins/libbasicplugin.so
 killall dde-dock
